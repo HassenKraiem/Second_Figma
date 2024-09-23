7262b41bcec9363bf54afcb3c755fff5649ae22b
hassen <kraiemhassen26@gmail.com>
2024-09-23 11:04:16 +0100

    What about now

--- app/src/main/java/com/example/secondfigma/MainActivity.kt
+++ app/src/main/java/com/example/secondfigma/MainActivity.kt
@@ -16,15 +16,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,6 +46,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,21 +56,64 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.res.FontResourcesParserCompat
+import com.example.secondfigma.ui.theme.DarkGrey
 import com.example.secondfigma.ui.theme.SecondFigmaTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SecondFigmaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AllViews(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CompositionLocalProvider(
+                LocalLayoutDirection provides LayoutDirection.Rtl
+            ) {
+                SecondFigmaTheme {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            CenterAlignedTopAppBar(
+                                title = {
+                                    Row(
+                                        horizontalArrangement = Arrangement.SpaceBetween,
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .background(color = Color.White)
+                                    ) {
+                                        Text(
+                                            text = "أذكار الصباح",
+                                            fontSize = 14.sp, modifier = Modifier.weight(1f),
+                                            fontWeight = FontWeight.Bold,
+                                            fontFamily = FontFamily(Font(R.font.cairobold)),
+                                            color = DarkGrey
+                                        )
+
+                                        IconButton(
+                                            onClick = { /*TODO*/ },
+                                            modifier = Modifier,
+                                        ) {
+                                            Image(
+                                                painter = painterResource(R.drawable.arrow___left),
+                                                contentDescription = null,
+                                                contentScale = ContentScale.Fit,
+                                            )
+                                        }
+                                    }
+                                },
+
+                                )
+                        },
+                    ) { innerPadding ->
+
+                        AllViews(
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
@@ -66,12 +121,15 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SecondFigmaTheme {
-        AllViews()
+    CompositionLocalProvider(
+        LocalLayoutDirection provides LayoutDirection.Rtl
+    ) {
+        SecondFigmaTheme {
+            AllViews()
+        }
     }
 }
 
@@ -79,34 +137,29 @@ fun GreetingPreview() {
 fun UpView(modifier: Modifier = Modifier) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
             .background(color = Color.White)
     ) {
-        Box {
-            Button(
-                onClick = { /*TODO*/ }, colors = ButtonColors(
-                    contentColor = Color.White,
-                    containerColor = Color.White,
-                    disabledContentColor = Color.Yellow,
-                    disabledContainerColor = Color.Red
-                ), modifier = Modifier.fillMaxWidth(0.25f),
-                border = BorderStroke(2.dp, Color.Black)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.arrow___left),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.fillMaxWidth(0.5f)
-                )
-            }
-        }
         Text(
             text = "أذكار الصباح",
-            fontSize = 25.sp, modifier = Modifier.fillMaxWidth(0.5f),
+            fontSize = 14.sp, modifier = Modifier.weight(1f),
             fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily(Font(R.font.cairobold))
+            fontFamily = FontFamily(Font(R.font.cairobold)),
+            color = DarkGrey
         )
+
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.arrow___left),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+            )
+        }
     }
 
 
@@ -118,53 +171,81 @@ fun DownView(modifier: Modifier = Modifier) {
     var pageNumber by remember { mutableStateOf(0) }
     Row(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(bottom = 50.dp), horizontalArrangement = Arrangement.SpaceAround
+            .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround
     ) {
-        Button(
-            onClick = {
-                if (pageNumber in (1..7)) {
-                    pageNumber--
-                }
-            }, colors = ButtonColors(
-                contentColor = Color.White,
-                containerColor = Color.White,
-                disabledContentColor = Color.Yellow,
-                disabledContainerColor = Color.Red
-            ), modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            border = BorderStroke(2.dp, Color.Black),
-            shape = RoundedCornerShape(25f)
-        ) {
-            Image(
-                painter = painterResource(R.drawable.arrow___left),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.fillMaxWidth()
+            Button(
+                onClick = {
+                    if (pageNumber in (1..7)) {
+                        pageNumber--
+                    }
+                }, colors = ButtonColors(
+                    contentColor = Color.White,
+                    containerColor = Color.White,
+                    disabledContentColor = Color.Yellow,
+                    disabledContainerColor = Color.Red
+                ), modifier = Modifier.padding(7.dp),
+                border = BorderStroke(1.dp, Color.Black),
+                shape = RoundedCornerShape(25f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.arrow___right),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                )
+            }
+        Box() {
+            Card(
+                modifier = Modifier.padding(5.dp)
+                    .width(60.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(25.dp),
+                border = BorderStroke(1.dp, color = colorResource(R.color.Asfar)),
+                colors = CardColors(
+                    containerColor = Color.Black,
+                    contentColor = colorResource(R.color.black),
+                    disabledContainerColor = Color.Red,
+                    disabledContentColor = Color.Red
+                )
+            ) {
+
+            }
+
+
+            Card(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(25.dp),
+                border = BorderStroke(1.dp, color = colorResource(R.color.Asfar)),
+                colors = CardColors(
+                    containerColor = colorResource(R.color.Asfar),
+                    contentColor = colorResource(R.color.black),
+                    disabledContainerColor = Color.Red,
+                    disabledContentColor = Color.Red
+                )
+
+
             )
+
+            {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 15.dp),
+                    text = "$pageNumber/7",
+                    fontFamily = FontFamily(Font(R.font.cairobold)),
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    lineHeight = 29.98.sp,
+                    color = colorResource(R.color.assouad1)
+                )
+
+
+            }
         }
 
-        Button(
-            onClick = {},
-            colors = ButtonColors(
-                containerColor = colorResource(R.color.Asfar),
-                contentColor = Color.Black,
-                disabledContainerColor = Color.Black,
-                disabledContentColor = Color.Black
-            ),
-            border = BorderStroke(2.dp, Color.Black),
-            shape = RoundedCornerShape(25f),
-            modifier = Modifier.padding(horizontal = 55.dp)
-        )
 
-        {
-            Text(
-                text = "$pageNumber/7",
-                fontFamily = FontFamily(Font(R.font.cairobold))
-            )
-
-        }
         Button(
             onClick = {
                 if (pageNumber in (0 until 7)) {
@@ -175,17 +256,15 @@ fun DownView(modifier: Modifier = Modifier) {
                 containerColor = Color.White,
                 disabledContentColor = Color.Yellow,
                 disabledContainerColor = Color.Red
-            ), modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            border = BorderStroke(2.dp, Color.Black),
+            ), modifier = Modifier.padding(top = 5.dp),
+            border = BorderStroke(1.dp, Color.Black),
             shape = RoundedCornerShape(25f)
         ) {
             Image(
-                painter = painterResource(R.drawable.arrow___right),
+                painter = painterResource(R.drawable.arrow___left),
                 contentDescription = null,
                 contentScale = ContentScale.Fit,
-                modifier = Modifier.fillMaxWidth(1f)
+                modifier = Modifier
             )
         }
 
@@ -196,27 +275,31 @@ fun DownView(modifier: Modifier = Modifier) {
 
 @Composable
 fun AllViews(modifier: Modifier = Modifier) {
-    Box(modifier = modifier
-        .fillMaxSize()
-        .padding(20.dp)) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
         Image(
             painter = painterResource(R.drawable.group_580),
             contentDescription = null,
-            contentScale = ContentScale.Fit,
+            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
-            UpView()
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceAround) {
             Text(
                 text = stringResource(R.string.main_text),
                 modifier = Modifier
-                    .fillMaxHeight(0.50f)
                     .fillMaxWidth()
+                    .height(266.dp)
+                    .padding(horizontal = 25.dp)
                     .border(2.dp, Color.Black, shape = RoundedCornerShape(15.dp))
-                    .safeContentPadding(),
+                    .padding(horizontal = 25.dp)
+                    .padding(top = 45.dp),
                 textAlign = TextAlign.Center,
-                fontSize = 25.sp,
-                fontFamily = FontFamily(Font(R.font.cairomedium))
+                fontSize = 14.sp,
+                lineHeight = 26.24.sp,
+                fontFamily = FontFamily(Font(R.font.cairomedium)),
+                color = colorResource(R.color.assouad1)
             )
             DownView()
         }
