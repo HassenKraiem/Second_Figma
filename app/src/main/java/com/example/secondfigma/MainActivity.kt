6330ac1810b9cbb190987ebda168952ebd41a6e7
hassen <kraiemhassen26@gmail.com>
2024-09-23 11:15:26 +0100

    What about now

--- app/src/main/java/com/example/secondfigma/MainActivity.kt
+++ app/src/main/java/com/example/secondfigma/MainActivity.kt
@@ -13,13 +13,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -28,22 +25,17 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -52,14 +44,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.res.FontResourcesParserCompat
 import com.example.secondfigma.ui.theme.DarkGrey
 import com.example.secondfigma.ui.theme.SecondFigmaTheme
 
@@ -87,7 +77,8 @@ class MainActivity : ComponentActivity() {
                                     ) {
                                         Text(
                                             text = "أذكار الصباح",
-                                            fontSize = 14.sp, modifier = Modifier.weight(1f),
+                                            fontSize = 14.sp,
+                                            modifier = Modifier.weight(1f),
                                             fontWeight = FontWeight.Bold,
                                             fontFamily = FontFamily(Font(R.font.cairobold)),
                                             color = DarkGrey
@@ -133,70 +124,40 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun UpView(modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = Color.White)
-    ) {
-        Text(
-            text = "أذكار الصباح",
-            fontSize = 14.sp, modifier = Modifier.weight(1f),
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily(Font(R.font.cairobold)),
-            color = DarkGrey
-        )
-
-        IconButton(
-            onClick = { /*TODO*/ },
-            modifier = Modifier,
-        ) {
-            Image(
-                painter = painterResource(R.drawable.arrow___left),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-            )
-        }
-    }
-
-
-}
-
 @SuppressLint("ResourceAsColor")
 @Composable
 fun DownView(modifier: Modifier = Modifier) {
-    var pageNumber by remember { mutableStateOf(0) }
+    var pageNumber by remember { mutableIntStateOf(0) }
     Row(
-        modifier = modifier
-            .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround
+        modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround
     ) {
-            Button(
-                onClick = {
-                    if (pageNumber in (1..7)) {
-                        pageNumber--
-                    }
-                }, colors = ButtonColors(
-                    contentColor = Color.White,
-                    containerColor = Color.White,
-                    disabledContentColor = Color.Yellow,
-                    disabledContainerColor = Color.Red
-                ), modifier = Modifier.padding(7.dp),
-                border = BorderStroke(1.dp, Color.Black),
-                shape = RoundedCornerShape(25f)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.arrow___right),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                )
-            }
-        Box() {
+        Button(
+            onClick = {
+                if (pageNumber in (1..7)) {
+                    pageNumber--
+                }
+            },
+            colors = ButtonColors(
+                contentColor = Color.White,
+                containerColor = Color.White,
+                disabledContentColor = Color.Yellow,
+                disabledContainerColor = Color.Red
+            ),
+            modifier = Modifier.padding(7.dp),
+            border = BorderStroke(1.dp, Color.Black),
+            shape = RoundedCornerShape(25f)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.arrow___right),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+            )
+        }
+        Box {
             Card(
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .padding(5.dp)
                     .width(60.dp)
                     .height(60.dp),
                 shape = RoundedCornerShape(25.dp),
@@ -251,12 +212,14 @@ fun DownView(modifier: Modifier = Modifier) {
                 if (pageNumber in (0 until 7)) {
                     pageNumber++
                 }
-            }, colors = ButtonColors(
+            },
+            colors = ButtonColors(
                 contentColor = Color.White,
                 containerColor = Color.White,
                 disabledContentColor = Color.Yellow,
                 disabledContainerColor = Color.Red
-            ), modifier = Modifier.padding(top = 5.dp),
+            ),
+            modifier = Modifier.padding(top = 5.dp),
             border = BorderStroke(1.dp, Color.Black),
             shape = RoundedCornerShape(25f)
         ) {
@@ -276,8 +239,7 @@ fun DownView(modifier: Modifier = Modifier) {
 @Composable
 fun AllViews(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier
-            .fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         Image(
             painter = painterResource(R.drawable.group_580),
